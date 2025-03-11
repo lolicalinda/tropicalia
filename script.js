9174a274d955c1e91d794c2758d3f138a97d12c4
elo <141043076+lolicalinda@users.noreply.github.com>
2025-03-11 09:46:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById
     ('aumentar-fonte');
-    let TamanhoAtualonte = 1;
+    const diminuirFonteBotao= document.getElementById
+    ('diminuir-fonte');
 
-    aumentaFonteBotao.addEventListener('click', funcion(){
-         tamanhoAtualFonte += 0.1,
-         documnt.body.fontSize = `${tamanhoAtualFonte}rem`
+    let tamanhoAtualFonte=1;
+
+    aumentaFonteBotao.addEventListener('click', function(){
+         tamanhoAtualFonte += 0.1;
+         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
       
     diminuirFonteBotao.addEventListener (`clik`, function(){
         tamanhoAtualFonte -= 0.1;
-        documnt.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
+
+const botaoDeAcessibilidade = document.getElementById('botao-acessbilidade');
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessiilidade');
+botaoDeAcessibilidade.addEventListener ('function', function(){
+    botaoDeAcessibilidade.classList.toggle('apresenta-lista');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lita')
+})
+
 })
 
